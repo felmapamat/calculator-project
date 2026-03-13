07f429699fb0b5742ed30e107ca2db2fb6f1b091
Felma F. Pamat <felma.pamat@nmsc.edu.ph>
2026-03-13 21:14:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
-function press(value){
-document.getElementById("display").value += value;
+let current = "";
+let operatorValue = "";
+let previous = "";
+
+function press(num){
+current += num;
+document.getElementById("display").value = current;
+}
+
+function operator(op){
+
+if(current === "") return;
+
+previous = current;
+operatorValue = op;
+
+document.getElementById("history").innerText = previous + " " + op;
+current = "";
+document.getElementById("display").value = "";
 }
 
 function calculate(){
-let display = document.getElementById("display");
 
-try{
-display.value = eval(display.value);
+let result;
+
+let prev = parseFloat(previous);
+let curr = parseFloat(current);
+
+switch(operatorValue){
+
+case "+":
+result = prev + curr;
+break;
+
+case "-":
+result = prev - curr;
+break;
+
+case "*":
+result = prev * curr;
+break;
+
+case "/":
+result = prev / curr;
+break;
+
 }
-catch{
-display.value = "Error";
-}
+
+document.getElementById("display").value = result;
+document.getElementById("history").innerText = previous + " " + operatorValue + " " + current;
+
+current = result.toString();
 }
 
 function clearDisplay(){
-document.getElementById("display").value = "";
+current="";
+previous="";
+operatorValue="";
+document.getElementById("display").value="";
+document.getElementById("history").innerText="";
 }
 
 function developer(){
@@ -22,5 +65,5 @@ alert("Developer: Felma Pamat");
 }
 
 function application(){
-alert("Application: Standard Calculator\nVersion: 1.0\nTechnology: HTML, CSS, JavaScript");
+alert("Application: Smart Web Calculator\nVersion: 1.0\nBuilt using HTML, CSS, JavaScript");
 }
